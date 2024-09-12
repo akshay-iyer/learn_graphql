e6876631c665cd131a48f1a11f0f33f96ddd13b2
Akshay Iyer <akshay.iyer@skyspecs.com>
2024-09-12 17:31:03 -0400

    dummy data

---
+++ DummyData.js
@@ -0,0 +1,32 @@
+const UserList = [
+    {
+        id: 1,
+        name: "Akshay",
+        username: "random9",
+        age: 31,
+        nationality: "India"
+    },
+    {
+        id: 2,
+        name: "Bendes",
+        username: "jbendes",
+        age: 33,
+        nationality: "USA"
+    },
+    {
+        id: 3,
+        name: "Amey",
+        username: "cooldude",
+        age: 29,
+        nationality: "India"
+    },
+    {
+        id: 4,
+        name: "Novak",
+        username: "nole",
+        age: 38,
+        nationality: "Serbia"
+    }
+];
+
+module.exports = { UserList }
