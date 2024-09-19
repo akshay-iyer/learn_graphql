e71470b548c78f54c74712f15e45e58bbf85bb93
Akshay Iyer <akshay.iyer@skyspecs.com>
2024-09-19 16:20:30 -0400

    adding new queries for users and movies

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,10 +1,36 @@
-const {UserList} = require('../DummyData');
+const { UserList, MovieList } = require('../DummyData');
+const lodash = require("lodash");
 
 const resolvers = {
     Query: {
-        users() { 
+        users: () => { 
             return UserList;
         },
+        
+        user: (_, args) => {
+            return lodash.find(UserList, {name: args.name})
+        },
+
+        friends: (_, args) => {
+            const name = args.name;
+            const user = lodash.find(UserList, {name:name})
+            return user.friends
+        },
+        
+        movies: () => {
+            return MovieList
+        },
+        
+        movie: (_, args) => {
+            const name = args.name;
+            const movie = lodash.find(MovieList, {name:name})
+            return movie
+        }
+    },
+    User: {
+        favoriteMovies: () => {
+            return lodash.filter(MovieList, (movie) => movie.isBlockbuster == true)
+        }
     },
 }
 
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -8,11 +8,33 @@ const typeDefs = gql`
         age: Int!
         nationality: String!
         friends: [User!]
+        favoriteMovies: [Movie!]
+    }
+
+    type Movie{
+        id: ID!
+        name: String!
+        year: Int!
+        isBlockbuster: Boolean!
     }
 
     type Query {
         users: [User!]!
-        friends: [User!]
+        user(name: String!): User!
+        friends(name : String!): [User!]
+        movies: [Movie!]!
+        movie(name: String!) : Movie!
+    }
+
+    input CreateUserInput{
+        name: String!
+        username: String!
+        age: Int!
+        nationality: String = "India"
+    }
+
+    type Mutation { 
+        createUser(input: CreateUserInput!): User!
     }
 `;
 
